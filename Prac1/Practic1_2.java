4441093a77cbe868a803dee04ca9389199cea85c
beomjun96 <80663025+beomjun96@users.noreply.github.com>
2021-10-15 09:39:32 +0900

    Add files via upload

---
+++ Prac1/Practic1_2.java
@@ -0,0 +1,19 @@
+/* 출
+TTTTTTTTTT
+TTTTTTTTTT
+    TT
+    TT
+    TT
+    */
+
+package Prac1;
+
+public class Practic1_2 {
+	public static void main(String[] args) {
+		System.out.println("T".repeat(10));
+		System.out.println("T".repeat(10));
+		System.out.println(" ".repeat(4)+"TT");
+		System.out.println(" ".repeat(4)+"TT");
+		System.out.println(" ".repeat(4)+"TT");
+	}
+}
---
+++ Prac1/Practice1_4.java
@@ -0,0 +1,25 @@
+// 세 개의 정수를 입력받아 합과 평균을 출력하는 프로그램을 작성하시오.
+// (단, 평균은 소수 이하를 버림하여 정수 부분만 출력)
+// 입력 20 50 100
+// 출력 sum = 170  avg = 56
+
+package Prac1;
+
+import java.util.*;
+public class Practice1_4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x,y,z,sum,avg;
+
+		x = input.nextInt();
+		y = input.nextInt();
+		z = input.nextInt();
+		sum = x+y+z;
+		avg = sum/3;
+		
+		System.out.println("sum=" + sum + "\navg=" +  avg);
+		
+		input.close();
+	}
+
+}
